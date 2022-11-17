37790012dff62d82bf0299966aa035400d1c5657
rodrigo <jrodrigo0592@gmail.com>
2022-11-17 18:55:03 -0300

    New Changes and features

--- 04-lists/for-forin/1.for-forin-forof.js
+++ 04-lists/for-forin/1.for-forin-forof.js
@@ -1,33 +1,33 @@
-const service = require('./service')
+const service = require("./service");
 
 async function main() {
-    try {
-        const result = await service.obterPessoas('a')
-        const names = []
+  try {
+    const result = await service.obterPessoas("a");
+    const names = [];
 
-        console.time('Tempo de execução (for padrão)')
-        for (let i=0; i <= result.results.length -1; i++) {
-            const pessoa = result.results[i]
-            names.push(pessoa.name)
-        }
-        console.timeEnd('Tempo de execução (for padrão)')
-
-        console.time('Tempo de execução (for in)')
-        for(let i in result.results) {
-            const pessoa = result.results[i]
-            names.push(pessoa.name)
-        }
-        console.timeEnd('Tempo de execução (for in)')
-
-        console.time('Tempo de execução (for of)')
-        for (pessoa of result.results) {
-            names.push(pessoa.name)
-        }
-        console.timeEnd('Tempo de execução (for of)')
-
-        console.log(names)
-    } catch (error) {
-        console.error("Erro interno", error)
+    console.time("Tempo de execução (for padrão)");
+    for (let i = 0; i <= result.results.length - 1; i++) {
+      const pessoa = result.results[i];
+      names.push(pessoa.name);
     }
+    console.timeEnd("Tempo de execução (for padrão)");
+
+    console.time("Tempo de execução (for in)");
+    for (let i in result.results) {
+      const pessoa = result.results[i];
+      names.push(pessoa.name);
+    }
+    console.timeEnd("Tempo de execução (for in)");
+
+    console.time("Tempo de execução (for of)");
+    for (pessoa of result.results) {
+      names.push(pessoa.name);
+    }
+    console.timeEnd("Tempo de execução (for of)");
+
+    console.log(names);
+  } catch (error) {
+    console.error("Erro interno", error);
+  }
 }
-main()
+main();
--- 04-lists/for-forin/3-filter.js
+++ 04-lists/for-forin/3-filter.js
@@ -1,17 +1,41 @@
-const { obterPessoas } = require('./service')
+const { obterPessoas } = require("./service");
+
+Array.prototype.meuFilter = function (callback) {
+  const lista = [];
+
+  for (index in this) {
+    const item = this[index];
+
+    const result = callback(item, index, this);
+    //  0. " ", null, undefined === false
+    if (!result) continue;
+
+    lista.push(item);
+  }
+
+  return lista;
+};
 
 async function main() {
-    try {
-        const { results } = await obterPessoas(`a`)
-        const familiaLars = results.filter(function (item) {
-            const result = item.name.toLowerCase().indexOf('lars') !== -1
-            return result
-        })
-        const names = familiaLars.map((pessoa) => pessoa.name)
-        console.log(names)
-    } catch (error) {
-        console.error('DEU RUIM', error)
-    }
+  try {
+    const { results } = await obterPessoas(`a`);
+
+    // const familyLars = results.filter(function (item) {
+    //   const result = item.name.toLowerCase().indexOf(`lars`) !== -1;
+    //   return result;
+    // });
+
+    const familyLars = results.meuFilter((item, index, lista) => {
+      console.log(`index: ${index}`, lista.length);
+      return item.name.toLowerCase().indexOf(`lars`) !== -1;
+    });
+
+    const names = familyLars.map((pessoa) => pessoa.name);
+
+    console.log(names);
+  } catch (error) {
+    console.error("DEU RUIM", error);
+  }
 }
 
-main()
+main();
